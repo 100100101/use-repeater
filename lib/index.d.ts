327d222a88a66e1fd8b6aa1f876dd885a0198ae0
Sergiy <bibik.serej@gmail.com>
2023-08-09 07:49:25 +0300

    степ

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,2 @@
-import { TRepeaterOptions, TRepeaterPromiseExtended } from '../types'
-export declare const useRepeater: (
-    options: TRepeaterOptions
-) => TRepeaterPromiseExtended
+import { TRepeaterOptions, TRepeaterPromiseExtended } from '../types';
+export declare const useRepeater: (options: TRepeaterOptions) => TRepeaterPromiseExtended;
